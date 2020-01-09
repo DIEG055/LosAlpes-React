063414f274e82409d6b5a92c00500bcd5fc78b7d
Juan Diego Medina <juanmedina0505@gmail.com>
2020-01-08 22:33:43 -0500

    project structure completed :tada:

---
+++ src/components/SignUpBox.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+import "./Styles/SignUpBox.scss";
+import googleIcon from "../static/images/icons/Google.svg" ;
+import facebookIcon from "../static/images/icons/Facebook.svg" ;
+
+class SignUpBox extends React.Component {
+  render() {
+
+    return (
+      <section className="loginBackground">
+        <div className="signUp">
+          <div className="signUp__container">
+            <h1>Regístrate</h1>
+            <form action="">
+              <div className="form__item">
+                <label for="Name">Nombre</label>
+                <input type="text" />
+              </div>
+              <div className="form__item">
+                <label for="Lastname">Apellido</label>
+                <input type="text" />
+              </div>
+              <div className="form__item">
+                <label for="Farm">Nombre Finca</label>
+                <input type="text" />
+              </div>
+              <div className="form__item">
+                <label for="email">Correo Electrónico</label>
+                <input type="email" />
+              </div>
+              <div className="form__item">
+                <label for="password">contraseña</label>
+                <input type="password" />
+              </div>
+              <div className="form__button">
+                <button>Registrarse</button>
+              </div>
+            </form>
+            <div className="redirectSignIn">
+              Ya tienes una cuenta? <a href="">Inicia Sesion</a>
+            </div>
+            <div className="SignIn__SocialMedia">
+              <div className="socialGoogle">
+                <img src={googleIcon} alt="Google" />
+              </div>
+              <div className="socialGoogle">
+                <img src={facebookIcon} alt="Facebook" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default SignUpBox;
